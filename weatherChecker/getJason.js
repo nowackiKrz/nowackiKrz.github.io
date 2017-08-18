358c608fd54f348f918828e93a597271bc6d5d57
Krzysztof Nowacki <krzysztof.nowacki@erecruiter.pl>
2017-08-18 16:08:36 +0200

    badge png changes

--- weatherChecker/getJason.js
+++ weatherChecker/getJason.js
@@ -257,7 +257,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // wywoluje fb share na click - funkcja pobierajace dane z fb id jest w <script> w html
     badgeClouds.addEventListener("click", function(event) {
             FB.ui({
-              method: 'share',
+              method: 'feed',
               href: 'https://nowackikrz.github.io/weatherChecker/index.html',
               name: 'I have earn an Cloud Destroyer Badge. Want some? Visit WeatherCheker',
               title: 'I have earn an Cloud Destroyer Badge. Want some? Visit WeatherCheker',
