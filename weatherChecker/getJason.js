f404a9a04983b72b097c11d59f032483447b02fc
Krzysztof Nowacki <krzysztof.nowacki@erecruiter.pl>
2017-08-19 18:51:30 +0200

    cookie split. rainow animation time change

--- weatherChecker/getJason.js
+++ weatherChecker/getJason.js
@@ -268,8 +268,11 @@ document.addEventListener("DOMContentLoaded", function() {
       display: 'iframe',
       href: 'https://nowackikrz.github.io/weatherChecker/images/badge%202.png',
       redirect_uri:'https://nowackikrz.github.io/weatherChecker/index.html',
+
+      /*
       hashtag:'weatherChecker',
       quote:'WeatherChecker confirmed. Im now officially a Cloud Destroyer!',
+      */
 
 
 
